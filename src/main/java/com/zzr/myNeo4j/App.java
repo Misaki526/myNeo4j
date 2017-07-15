50db23e4701e19c425d32aed7c17f1e48773c767
Misaki526 <misaki_526@163.com>
2017-07-15 22:35:00 +0800

    misaki_test

--- src/main/java/com/zzr/myNeo4j/App.java
+++ src/main/java/com/zzr/myNeo4j/App.java
@@ -7,7 +7,6 @@ import java.sql.Statement;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
